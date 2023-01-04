ac5b0f957171daf985c23f5aa16419e9791f07f2
unknownbreaker <6026454+unknownbreaker@users.noreply.github.com>
2023-01-04 10:51:17 -0500

    Fix for 2023

--- standup-checker.js
+++ standup-checker.js
@@ -1,25 +1,48 @@
 var moment = require("moment");
 
-var needsSlack = false;
-var weeksNeeded = {
+let needsSlack = false;
+let weeksNeeded = {
   weds: [],
   thurs: [],
 };
-var todayDate = moment().format("dddd YYYY-MM-DD");
-var currentDayOfWeek = moment().day();
-
+const todayDate = moment().format("dddd YYYY-MM-DD");
+const currentDayOfWeek = moment().day();
 if (currentDayOfWeek === 5) needsSlack = true;
-
 switch (moment().month()) {
+  case 0:
+    weeksNeeded.thurs.push(...[2, 4]);
+    weeksNeeded.weds.push(...[1, 3]);
+    break;
+  case 1:
+    weeksNeeded.thurs.push(...[2, 4]);
+    weeksNeeded.weds.push(...[1, 3]);
+    break;
+  case 2:
+    weeksNeeded.thurs.push(...[2, 4]);
+    weeksNeeded.weds.push(...[1, 3, 5]);
+    break;
+  case 3:
+    weeksNeeded.thurs.push(...[1, 3]);
+    weeksNeeded.weds.push(...[2, 4]);
+    break;
+  case 4:
+    weeksNeeded.thurs.push(...[1, 3]);
+    weeksNeeded.weds.push(...[2, 4]);
+    break;
+  case 5:
+    weeksNeeded.thurs.push(...[1, 3, 5]);
+    weeksNeeded.weds.push(...[1, 3]);
+    break;
   case 6:
-    weeksNeeded.thurs.push(4);
+    weeksNeeded.thurs.push(...[2, 4]);
+    weeksNeeded.weds.push(...[1, 3]);
     break;
   case 7:
     weeksNeeded.weds.push(...[1, 3, 5]);
     weeksNeeded.thurs.push(...[2, 4]);
     break;
   case 8:
-    weeksNeeded.thurs.push(...[2, 4]);
+    weeksNeeded.thurs.push(...[1, 3]);
     weeksNeeded.weds.push(...[2, 4]);
     break;
   case 9:
@@ -27,16 +50,15 @@ switch (moment().month()) {
     weeksNeeded.weds.push(...[2, 4]);
     break;
   case 10:
-    weeksNeeded.thurs.push(...[1, 3]);
+    weeksNeeded.thurs.push(...[1, 3, 5]);
     weeksNeeded.weds.push(...[2, 4]);
     break;
-  case 11:
+  case 11: // dec from 2022
     weeksNeeded.thurs.push(...[1, 3, 5]);
     weeksNeeded.weds.push(...[1, 3]);
     break;
 }
-
-var days = {
+const days = {
   Mon: 1,
   Tue: 2,
   weds: 3,
@@ -45,8 +67,7 @@ var days = {
   Sat: 6,
   Sun: 7,
 };
-
-var nthDayOfMonth = (monthMoment, day, weekNumber) => {
+const nthDayOfMonth = (monthMoment, day, weekNumber) => {
   let m = monthMoment.clone().startOf("month").day(day);
   if (m.month() !== monthMoment.month()) m.add(7, "d");
   return m.add(7 * (weekNumber - 1), "d").format("dddd YYYY-MM-DD");
